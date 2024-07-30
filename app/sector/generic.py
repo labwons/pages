e58f9bbbec3c8697437b885a68b9ec3ddc527f40
Jehoshaphat-kr <jhlee_0319@naver.com>
2024-07-30 11:04:39 +0900

    Test Release

--- app/sector/generic.py
+++ app/sector/generic.py
@@ -9,26 +9,24 @@ import pandas, os
 
 class Wise(DataFrame):
     
-    def __init__(self, index:str):
+    def __init__(self, index:str, read_only:bool=False):
         _date = fetch.index_date()
         _name = fetch.index_name(index)
         _path = os.path.join(os.path.dirname(__file__), rf'archive/{_name.lower()}.json')
         _code = fetch.KEYS[_name]
         
-        try:
+        if read_only:
             super().__init__(pandas.read_json(_path, orient='index'))
-        except (FileExistsError, FileNotFoundError, ValueError):
-            super().__init__()
-            
-        if self.empty:
-            super().__init__(
-                pandas.concat(
-                    objs=[fetch.index_component(_date, cd) for cd in _code],
-                    axis=0,
-                    ignore_index=False
-                )
+            return
+        
+        super().__init__(
+            pandas.concat(
+                objs=[fetch.index_component(_date, cd) for cd in _code],
+                axis=0,
+                ignore_index=False
             )
-            self.to_json(_path, orient='index')        
+        )
+        self.to_json(_path, orient='index')
         return   
     
     
