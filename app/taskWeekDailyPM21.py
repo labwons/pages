e4a63302304d54d6c86bc873a74b70a68b9d5dd2
Jehoshaphat-kr <jhlee_0319@naver.com>
2024-07-30 10:16:04 +0900

    Test Release

--- app/taskWeekDailyPM21.py
+++ app/taskWeekDailyPM21.py
@@ -1,21 +1,7 @@
 try:
-    from .common import PATH
     from .sector.generic import Wise
-    from .sector.fetch import KEY
 except ImportError:
-    from app.common import PATH
     from app.sector.generic import Wise
-    from app.sector.fetch import KEY
 
 if __name__ == "__main__":
-    Wise(KEY.WI26) \
-    .to_json(
-        path_or_buf=PATH.JSONWI26, 
-        orient='index'
-    )
-    
-    Wise(KEY.WICS) \
-    .to_json(
-        path_or_buf=PATH.JSONWICS, 
-        orient='index'
-    )
+    Wise('WICS'); Wise('WI26')
