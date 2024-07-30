765d569903f22f5018694416ae16d46e55bbeb2d
Jehoshaphat-kr <jhlee_0319@naver.com>
2024-07-30 11:37:18 +0900

    Test Release

--- app/sector/generic.py
+++ app/sector/generic.py
@@ -9,32 +9,30 @@ import pandas, os
 
 class Wise(DataFrame):
     
-    def __init__(self, index:str, read_only:bool=False):
+    def __init__(self, index:str, auto_update:bool=False):
         _date = fetch.index_date()
         _name = fetch.index_name(index)
         _code = fetch.KEYS[_name]
         _path = os.path.join(os.path.dirname(__file__), rf'archive/{_name.lower()}.json')
         _root = f"https://raw.githubusercontent.com/labwons/pages/main/app/sector/archive/{_name.lower()}.json"
         
-        if read_only:
-            try:
-                super().__init__(pandas.read_json(_path, orient='index'))
-            except (FileExistsError, FileNotFoundError):
-                super().__init__(pandas.read_json(_root, orient='index'))
-            return
-        
-        super().__init__(
-            pandas.concat(
-                objs=[fetch.index_component(_date, cd) for cd in _code],
-                axis=0,
-                ignore_index=False
+        if auto_update:
+            super().__init__(
+                pandas.concat(
+                    objs=[fetch.index_component(_date, cd) for cd in _code],
+                    axis=0,
+                    ignore_index=False
+                )
             )
-        )
-        self.to_json(_path, orient='index')
-        return   
+            self.to_json(_path, orient='index')
+            return
+        try:
+            super().__init__(pandas.read_json(_path, orient='index'))
+        except (FileExistsError, FileNotFoundError):
+            super().__init__(pandas.read_json(_root, orient='index'))
+        return        
     
     
 if __name__ == "__main__":
-
     
     print(Wise('WICS'))
--- app/taskWeekDailyPM21.py
+++ app/taskWeekDailyPM21.py
@@ -3,5 +3,7 @@ try:
 except ImportError:
     from app.sector.generic import Wise
 
+
 if __name__ == "__main__":
-    Wise('WICS'); Wise('WI26')
+    Wise('WICS', auto_update=True)
+    Wise('WI26', auto_update=True)
