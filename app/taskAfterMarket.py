45cdbbd5f5f033e3c2853d3fbefbcac9ea696fa2
Jehoshaphat-kr <jhlee_0319@naver.com>
2024-08-02 17:24:45 +0900

    Test Release

--- app/taskAfterMarket.py
+++ app/taskAfterMarket.py
@@ -1,18 +1,17 @@
 try:
-    # from .barmap.generic import BarMap
-    from .market.generic import Market
-    from .sector.generic import Wise
+    from .barmap.generic import MarketMap
 except ImportError:
-    # from app.barmap.generic import BarMap
-    from app.market.generic import Market
-    from app.sector.generic import Wise
+    from app.barmap.generic import MarketMap
 
 
 if __name__ == "__main__":
-    wics = Wise('wics', auto_update=False)
-    wi26 = Wise('wi26', auto_update=False)
-    market = Market(auto_update=True)
-    
+    WICS = MarketMap('WICS', update_index=False, update_market=True)
+    WI26 = MarketMap('WI26', update_index=False, update_market=False)
+    bo, bc = "{", "}"
+    with open(r"./barmap/archive/marketmap.json", mode="w") as file:
+        file.write(f"""
+
+""")
     
     
     # print(wics.join(market, how='left'))
