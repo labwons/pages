1fc631768db0a84e741ae1cf00300e17ac746e14
Jehoshaphat-kr <jhlee_0319@naver.com>
2024-08-13 17:02:00 +0900

    Test Release

--- apps/marketmap/src/js/treemap.js
+++ apps/marketmap/src/js/treemap.js
@@ -1,86 +1,107 @@
-// const __URL__ = 'https://raw.githubusercontent.com/labwons/pages/main/src/json/treemap/treemap.json';
-const __URL__ = '../../../../src/json/treemap/treemap.json';
-const __ID__ = 'market-map';
+const __URL__ = 'https://raw.githubusercontent.com/labwons/pages/main/src/json/treemap/treemap.json';
+// const __URL__ = '../../../../src/json/treemap/treemap.json';
+
 
 let __SRC__ = null;
 
-
+var base = null;
 var data = null;
 var spec = null;
+var tops = null;
 
 
-function reset_search(){
+function setTops(){
+  tops = [];
+  for (var n = base.meta.length - 2; n > 0; n--) {
+    tops.push(base.name[n]);
+    if (base.meta[n].includes('종가')) { return tops; }
+  }
+  return [];
+}
+
+function searchReset(){
 	$('.map-keyin').empty();
 	$('.map-keyin').append('<option></option>');
-	for (var n = 0; n < data.name.length; n++){
-		$('.map-keyin').append('<option>' + data.name[n] + '</option>');
+	for (var n = 0; n < base.name.length; n++){
+		$('.map-keyin').append('<option>' + base.name[n] + '</option>');
 	}
 }
 
-// function search_top(__group__){
-//   var elems = $('.slicetext');
-// 	var clicker = null;
-// 	var name = '';
-// 	for (var n = 0; n < elems.length; n++){
-// 		name = $(elems[n]).text();
-// 		if (name == __group__){
-// 			clicker = $(elems[n]).parent().get(0);
-// 			break;
-// 		}
-// 	}
-// 	if (clicker == null) {return;}
-// 	if (document.createEvent) {
-// 			var event = document.createEvent('MouseEvents');
-// 			event.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-// 			!clicker.dispatchEvent(event);
-// 	} else if (link.fireEvent) {
-// 		!clicker.fireEvent('onclick');
-// 	}
-// }
-
-// function search_asset(__asset_name){
-// 	var elems = $('.slicetext');
-// 	var clicker = null;
-// 	for (var n = 0; n < elems.length; n++){
-// 		var name = $(elems[n]).text();
-//     if ((name.includes(__asset_name)) && (name.slice(0, __asset_name.length) == __asset_name)){
-//       var leftover = name.slice(__asset_name.length, name.length)
-//       var tail_cnt = 0;
-//       var digits = leftover.match(/\d/g);
-//       if (digits != null){
-//   			tail_cnt += digits.length;
-//   		}
-//       if (leftover.includes('-')){ tail_cnt += 1 }
-//       if (leftover.includes('.')){ tail_cnt += 1 }
-//       if (leftover.includes('%')){ tail_cnt += 1 }
-//       if ((leftover.length - tail_cnt) == 0){
-//         clicker = $(elems[n]).parent().get(0);
-//   		  break;
-//       }
+// function searchObj(name) {
+//   var objs = $('.slicetext');
+//   for (var n = 0; n < objs.length; n++) {
+//     if ($(objs[n]).text().includes(name)) {
+//       return objs[n];
 //     }
-// 	}
-// 	if (clicker == null){
-// 		return
-// 	}
-// 	if (document.createEvent) {
-// 			var event = document.createEvent('MouseEvents');
-// 			event.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-// 			!clicker.dispatchEvent(event);
-// 	} else if (link.fireEvent) {
-// 		!clicker.fireEvent('onclick');
-// 	}
+//   }
 // }
 
-function update_map(id) {
+function searchTop(top){
+  var elems = $('.slicetext');
+	var clicker = null;
+	var name = '';
+	for (var n = 0; n < elems.length; n++){
+		name = $(elems[n]).text();
+		if (name == top){
+			clicker = $(elems[n]).parent().get(0);
+			break;
+		}
+	}
+	if (clicker == null) {return;}
+	if (document.createEvent) {
+			var event = document.createEvent('MouseEvents');
+			event.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+			!clicker.dispatchEvent(event);
+	} else if (link.fireEvent) {
+		!clicker.fireEvent('onclick');
+	}
+}
+
+function searchAsset(asset){
+	var elems = $('.slicetext');
+	var clicker = null;
+	for (var n = 0; n < elems.length; n++){
+		var name = $(elems[n]).text();
+    if ((name.includes(asset)) && (name.slice(0, asset.length) == asset)){
+      var leftover = name.slice(asset.length, name.length)
+      var tail_cnt = 0;
+      var digits = leftover.match(/\d/g);
+      if (digits != null){
+  			tail_cnt += digits.length;
+  		}
+      if (leftover.includes('-')){ tail_cnt += 1 }
+      if (leftover.includes('.')){ tail_cnt += 1 }
+      if (leftover.includes('%')){ tail_cnt += 1 }
+      if ((leftover.length - tail_cnt) == 0){
+        clicker = $(elems[n]).parent().get(0);
+  		  break;
+      }
+    }
+	}
+	if (clicker == null){
+		return
+	}
+  console.log(clicker);
+  $(clicker).css("border", "3px solid white");
+	// if (document.createEvent) {
+	// 		var event = document.createEvent('MouseEvents');
+	// 		event.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+	// 		!clicker.dispatchEvent(event);
+	// } else if (link.fireEvent) {
+	// 	!clicker.fireEvent('onclick');
+	// }
+}
+
+function update_map() {
   unit = (spec == 'PER' || spec == 'PBR') ? '' : '%';
-  kwargs = {
+  data = {
     type:'treemap',
     branchvalues:'total',
-    labels:data.name,
-    parents:data.cover,
-    values:data.size,
-    meta:data.meta,
-    text:data[spec],
+    labels:base.name,
+    parents:base.cover,
+    values:base.size,
+    meta:base.meta,
+    text:base[spec],
 	  textposition:'middle center',
     textfont:{
       family:'NanumGothic, Nanum Gothic, monospace',
@@ -96,14 +117,14 @@ function update_map(id) {
     },
     opacity: 0.9,
     marker: {
-      colors: data[spec + '-C'],
+      colors: base[spec + '-C'],
       visible: true
     },
     root_color:'lightgrey'
   }
   Plotly.newPlot(
-    id, 
-    [kwargs],
+    'market-map', 
+    [data],
     {
       height: 650,
       margin:{l:0,r:0,t:0,b:25}
@@ -117,6 +138,107 @@ function update_map(id) {
 }
 
 
+$('.map-option').on('change', function() {
+  spec = $('.map-option').val();
+  if ((spec == 'PER') || (spec == 'PBR')) {
+    $('.maps-lowest').html('고평가');
+    $('.maps-lower').html('');
+    $('.maps-low').html('');
+    $('.maps-middle').html('평균');
+    $('.maps-high').html('');
+    $('.maps-higher').html('');
+    $('.maps-highest').html('저평가');
+  } else if (spec == 'DIV') {
+    $('.maps-lowest').html('');
+    $('.maps-lower').html('');
+    $('.maps-low').html('');
+    $('.maps-middle').html('0%');
+    $('.maps-high').html('2%');
+    $('.maps-higher').html('4%');
+    $('.maps-highest').html('6%');
+  } else if (spec == 'D-1') {
+    $('.maps-lowest').html('-3%');
+    $('.maps-lower').html('-2%');
+    $('.maps-low').html('-1%');
+    $('.maps-middle').html('0%');
+    $('.maps-high').html('1%');
+    $('.maps-higher').html('2%');
+    $('.maps-highest').html('3%');
+  } else if (spec == 'W-1') {
+    $('.maps-lowest').html('-6%');
+    $('.maps-lower').html('-4%');
+    $('.maps-low').html('-2%');
+    $('.maps-middle').html('0%');
+    $('.maps-high').html('2%');
+    $('.maps-higher').html('4%');
+    $('.maps-highest').html('6%');
+  } else if (spec == 'M-1') {
+    $('.maps-lowest').html('-10%');
+    $('.maps-lower').html('-6.7%');
+    $('.maps-low').html('-3.3%');
+    $('.maps-middle').html('0%');
+    $('.maps-high').html('3.3%');
+    $('.maps-higher').html('6.7%');
+    $('.maps-highest').html('10%');
+  } else if (spec == 'M-3') {
+    $('.maps-lowest').html('-18%');
+    $('.maps-lower').html('-12%');
+    $('.maps-low').html('-6%');
+    $('.maps-middle').html('0%');
+    $('.maps-high').html('6%');
+    $('.maps-higher').html('12%');
+    $('.maps-highest').html('18%');
+  } else if (spec == 'M-6') {
+    $('.maps-lowest').html('-24%');
+    $('.maps-lower').html('-16%');
+    $('.maps-low').html('-8%');
+    $('.maps-middle').html('0%');
+    $('.maps-high').html('8%');
+    $('.maps-higher').html('16%');
+    $('.maps-highest').html('24%');
+  } else if (spec == 'Y-1') {
+    $('.maps-lowest').html('-30%');
+    $('.maps-lower').html('-20%');
+    $('.maps-low').html('-10%');
+    $('.maps-middle').html('0%');
+    $('.maps-high').html('10%');
+    $('.maps-higher').html('20%');
+    $('.maps-highest').html('30%');
+  }
+  update_map(__ID__);
+})
+
+$('.map-keyin').on('select2:select', function (e) {
+    var _select = e.params.data.text;
+    if (_select == "") { return }
+
+    if (tops.includes(_select)) {
+      var group = _select;
+    } else{
+      var group = base.cover[base.name.indexOf(_select)];
+    }
+    searchTop(group);
+
+    if (_select == group){ return }
+    searchAsset(_select);
+    // setTimeout(function(){
+    //   searchAsset(username);
+    //   $('.map-rewind').toggleClass('show');
+    // }, 1000)
+});
+
+$('.map-reset').click(function(){
+    update_map();
+})
+
+$('.map-rewind').click(function(){
+  $(this).toggleClass('show');
+})
+
+// $('#market-map').on('plotly_click', function(e, d){
+//   console.log(d);
+// })
+
 $(document).ready(async function(){
 
   try {
@@ -129,10 +251,11 @@ $(document).ready(async function(){
       console.error('Fetch error:', error);
   }
 
-  data = __SRC__[$('.map-type').val()];
+  base = __SRC__[$('.map-type').val()];
   spec = $('.map-option').val();
-  update_map(__ID__);
-  reset_search();
+  tops = setTops();
+  update_map();
+  searchReset();
 })
 
 
@@ -141,113 +264,14 @@ $(document).ready(function() {
   if ($('#header').attr("class").includes("header-fix")) {
     $('#header').removeClass('header-fix');
   }
-
   $('.map-keyin').select2({
-    placeholder: "종목명 검색"
+    placeholder: "Testing"
   })
-
-  $('.map-type').on('change', function() {
-    data = __SRC__[$('.map-type').val()];
-    update_map(__ID__);
-  })
-
-  $('.map-option').on('change', function() {
-    spec = $('.map-option').val();
-    if ((spec == 'PER') || (spec == 'PBR')) {
-      $('.maps-lowest').html('고평가');
-      $('.maps-lower').html('');
-      $('.maps-low').html('');
-      $('.maps-middle').html('평균');
-      $('.maps-high').html('');
-      $('.maps-higher').html('');
-      $('.maps-highest').html('저평가');
-    } else if (spec == 'DIV') {
-      $('.maps-lowest').html('');
-      $('.maps-lower').html('');
-      $('.maps-low').html('');
-      $('.maps-middle').html('0%');
-      $('.maps-high').html('2%');
-      $('.maps-higher').html('4%');
-      $('.maps-highest').html('6%');
-    } else if (spec == 'D-1') {
-      $('.maps-lowest').html('-3%');
-      $('.maps-lower').html('-2%');
-      $('.maps-low').html('-1%');
-      $('.maps-middle').html('0%');
-      $('.maps-high').html('1%');
-      $('.maps-higher').html('2%');
-      $('.maps-highest').html('3%');
-    } else if (spec == 'W-1') {
-      $('.maps-lowest').html('-6%');
-      $('.maps-lower').html('-4%');
-      $('.maps-low').html('-2%');
-      $('.maps-middle').html('0%');
-      $('.maps-high').html('2%');
-      $('.maps-higher').html('4%');
-      $('.maps-highest').html('6%');
-    } else if (spec == 'M-1') {
-      $('.maps-lowest').html('-10%');
-      $('.maps-lower').html('-6.7%');
-      $('.maps-low').html('-3.3%');
-      $('.maps-middle').html('0%');
-      $('.maps-high').html('3.3%');
-      $('.maps-higher').html('6.7%');
-      $('.maps-highest').html('10%');
-    } else if (spec == 'M-3') {
-      $('.maps-lowest').html('-18%');
-      $('.maps-lower').html('-12%');
-      $('.maps-low').html('-6%');
-      $('.maps-middle').html('0%');
-      $('.maps-high').html('6%');
-      $('.maps-higher').html('12%');
-      $('.maps-highest').html('18%');
-    } else if (spec == 'M-6') {
-      $('.maps-lowest').html('-24%');
-      $('.maps-lower').html('-16%');
-      $('.maps-low').html('-8%');
-      $('.maps-middle').html('0%');
-      $('.maps-high').html('8%');
-      $('.maps-higher').html('16%');
-      $('.maps-highest').html('24%');
-    } else if (spec == 'Y-1') {
-      $('.maps-lowest').html('-30%');
-      $('.maps-lower').html('-20%');
-      $('.maps-low').html('-10%');
-      $('.maps-middle').html('0%');
-      $('.maps-high').html('10%');
-      $('.maps-higher').html('20%');
-      $('.maps-highest').html('30%');
-    }
-    update_map(__ID__);
-  })
-
 })
 
-
-//   // MAP Search
-//   $('.map-search').on('select2:select', function (e) {
-//       var username = e.params.data.text;
-//       if (username == "") { return }
-
-//       var i_start = username.indexOf('[');
-//       if (i_start != -1){
-//       var group = username.slice(i_start + 1, username.length - 1);
-//       username = username.slice(0, i_start);
-//       } else if (group_data.includes(username)) {
-//       var group = username;
-//       } else{
-//       var idx = tdat_name.indexOf(username);
-//       var group = tdat_covers[map_key][idx];
-//       }
-
-//       search_top(group);
-//       if (username == group){return}
-//       setTimeout(function(){
-//       search_asset(username);
-//       }, 1000)
-//   });
-
-  // MAP Reset
-  $('#map-reset').click(function(){
-      update_map();
-  })
+$('.map-type').on('change', function() {
+  base = __SRC__[$('.map-type').val()];
+  tops = setTops();
+  update_map(__ID__);
+  searchReset();
+})
