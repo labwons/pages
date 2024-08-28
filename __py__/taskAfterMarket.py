ce747ce5cca226acfb5c564d80a291a3e83c4395
Jehoshaphat-kr <jhlee_0319@naver.com>
2024-08-28 17:04:09 +0900

    Test Release

--- __py__/taskAfterMarket.py
+++ __py__/taskAfterMarket.py
@@ -6,20 +6,30 @@ except ImportError:
     from __py__.deploy.sitemap import updateSitemap, updateRSS
     from __py__.treemap.generic import MarketMap    
     from __py__.rank.generic import Rank
-import os
+import os, json
 
 
 if __name__ == "__main__":
-    bo, bc = "{", "}"
+    
+    treemap = "{" + json.dumps(json.loads(str(MarketMap(True))), separators=(',', ':')) + "}"
+    ranking = "{" + json.dumps(json.loads(str(Rank())), separators=(',', ':')) + "}"
     with open(os.path.join(os.path.dirname(__file__), r"../src/json/treemap/treemap.json"), mode="w") as file:
-        file.write(f"""{bo}
-{MarketMap(auto_update=True)}
-{bc}""")
-        
+        file.write(treemap)
     with open(os.path.join(os.path.dirname(__file__), r"../src/json/rank/rank.json"), mode="w") as file:
-        file.write(f"""{bo}
-{Rank()}
-{bc}""")
+        file.write(ranking)
+    
+#     bo, bc = "{", "}"
+#     with open(os.path.join(os.path.dirname(__file__), r"../src/json/treemap/treemap.json"), mode="w") as file:
+#         file.write(f"""{bo}
+# {MarketMap(auto_update=True)}
+# {bc}""")
+        
+#     with open(os.path.join(os.path.dirname(__file__), r"../src/json/rank/rank.json"), mode="w") as file:
+#         file.write(f"""{bo}
+# {Rank()}
+# {bc}""")
+
+
     updateSitemap()
     updateRSS()
     print("Success.")
