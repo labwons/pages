fd053e79d8a9e50804244c950573d58ddba7ed36
labwons <122770260+labwons@users.noreply.github.com>
2025-06-23 22:03:41 +0900

    Test release

--- src/common/util.py
+++ src/common/util.py
@@ -3,7 +3,7 @@ from pandas import isna
 from typing import Union
 
 
-def krw2currency(krw: int) -> Union[str, float]:
+def krw2currency(krw: int, limit:str='') -> Union[str, float]:
     """
     KRW (원화) 입력 시 화폐 표기 법으로 변환(자동 계산)
     @krw 단위는 원 일 것
@@ -15,6 +15,8 @@ def krw2currency(krw: int) -> Union[str, float]:
         return f'{int(krw // 10000)}조 {int(krw % 10000)}억'
     if krw >= 1e+8:
         krw /= 1e+4
+        if limit == '억':
+            return f'{int(krw // 10000)}억'
         return f'{int(krw // 10000)}억 {int(krw % 10000)}만'
     return f'{int(krw // 10000)}만'
 
