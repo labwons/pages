77701d5135055f67b29829f015c89043e3e22bfe
= <JeHyeuk.Lee@hyundai-kefico.com>
2025-08-25 09:57:54 +0900

    TEST ACTION

--- src/labwons/fetch/stock/naver.py
+++ src/labwons/fetch/stock/naver.py
@@ -1,20 +1,42 @@
-try:
-    from ..util import web, str2num
-except ImportError:
-    from src.fetch.util import web, str2num
+from labwons.util import DP
+from labwons.logs import fetch_logger as logger
+from bs4 import BeautifulSoup
 from pandas import DataFrame, Series
 from typing import Union
+import pandas as pd
+import requests
 
 
 class naver:
     """
     Fetch source data from Naver
+    """
 
-    @currentPrice
+    def __init__(self, ticker:str, log:bool=True, ignore_error:bool=False):
+        self._req = _req = requests.get(f"https://finance.naver.com/item/main.naver?code={ticker}", verify=False)
+        if not _req.status_code == 200:
+            if log:
+                logger.error(f'Connection Error: {ticker}')
+            if not ignore_error:
+                raise ConnectionError(f'Connection Error: {ticker}')
+            return
+
+        self._tables = pd.read_html(_req.text)
+        return
+
+    @property
+    def currentPrice(self) -> Union[int, float]:
+        """
         type        : Union[int, float]
         description : current price (close)
+        """
+        html = BeautifulSoup(self._req.text, 'lxml')
+        curr = [d.text for d in html.find_all("dd") if d.text.startswith("현재가")][0]
+        return DP.str2num(curr[curr.index("현재가 ") + 4: curr.index(" 전일대비")])
 
-    @resemblances
+    @property
+    def resemblances(self) -> DataFrame:
+        """
         type        : DataFrame
         description : current price (close)
         columns     : ['종목명', '현재가', '등락률', '시가총액(억)', '외국인비율(%)', '매출액(억)', '영업이익(억)',
@@ -27,8 +49,20 @@ class naver:
             000660  SK하이닉스  132000   1.15       960963         52.59       73059       -28821  ...  -11.73    1.58
             402340    SK스퀘어   48450   0.41        67336         45.29       -1274        -7345  ...   -3.64    0.43
             042700  한미반도체   60200  -9.20        58598         12.38         491          112  ...   29.07   10.80
+        """
+        data = self._tables[5]
+        data = data.set_index(keys='종목명').drop(index=['전일대비'])
+        data.index.name = None
+        for col in data:
+            data[col] = data[col].apply(DP.str2num)
+        data = data.T
+        data["종목명"] = [i.replace('*', '')[:-6] for i in data.index]
+        data.index = [i[-6:] for i in data.index]
+        return data
 
-    @multiplesTrailing
+    @property
+    def trailingMultiples(self) -> Series:
+        """
         type        : Series
         description : trailing multiples
         example     :
@@ -40,49 +74,19 @@ class naver:
             bookValue      416754.00
             dividendYield        2.1
             dtype: float64
-
-    @currentPrice
-        type        : Union[int, float]
-        description : current price (close)
-    """
-
-    def __init__(self, ticker:str):
-        self._url_main = f"https://finance.naver.com/item/main.naver?code={ticker}"
-        return
-
-    @property
-    def currentPrice(self) -> Union[int, float]:
-        html = web.html(self._url_main)
-        curr = [d.text for d in html.find_all("dd") if d.text.startswith("현재가")][0]
-        return str2num(curr[curr.index("현재가 ") + 4: curr.index(" 전일대비")])
-
-    @property
-    def resemblances(self) -> DataFrame:
-        tables = web.list(self._url_main)
-        data = web.list(self._url_main)[5]
-        data = data.set_index(keys='종목명').drop(index=['전일대비'])
-        data.index.name = None
-        for col in data:
-            data[col] = data[col].apply(str2num)
-        data = data.T
-        data["종목명"] = [i.replace('*', '')[:-6] for i in data.index]
-        data.index = [i[-6:] for i in data.index]
-        return data
-
-    @property
-    def trailingMultiples(self) -> Series:
-        data = web.list(self._url_main)[9]
-        per, eps = map(str, data.columns[-1].split('l'))
-        estPE, estEps = map(str, data.iloc[0, -1].split('l'))
-        pbr, bps = map(str, data.iloc[1, -1].split('l'))
+        """
+        data = self._tables[9]
+        per, eps = map(str, data.iloc[0, -1].split('l'))
+        estPE, estEps = map(str, data.iloc[1, -1].split('l'))
+        pbr, bps = map(str, data.iloc[2, -1].split('l'))
         return Series({
-            "trailingPE": str2num(per),
-            "trailingEps": str2num(eps),
-            "estimatePE": str2num(estPE),
-            "estimateEps": str2num(estEps),
-            "priceToBook": str2num(pbr),
-            "bookValue": str2num(bps),
-            "dividendYield": str2num(data.iloc[-1, -1])
+            "trailingPE": DP.str2num(per),
+            "trailingEps": DP.str2num(eps),
+            "estimatePE": DP.str2num(estPE),
+            "estimateEps": DP.str2num(estEps),
+            "priceToBook": DP.str2num(pbr),
+            "bookValue": DP.str2num(bps),
+            "dividendYield": DP.str2num(data.iloc[-1, -1])
         })
 
 
